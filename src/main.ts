faa3646fd427ed4a74bd60c21b193876b33be56f
JimiC <jimikar@gmail.com>
2017-02-28 16:11:14 +0200

    Update @angular/cli to rc0.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AppModule } from './app/';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
